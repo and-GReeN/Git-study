6227f00a2a4adb22d0e405dfbf5ae62280994b3a
GReeN <andgreen@bk.ru>
2020-08-18 13:07:17 +0300

    fix test4 ;

--- src/test4.java
+++ src/test4.java
@@ -2,6 +2,6 @@ public class test4
 {
 public static void main(String[] args)
 {
-System.out.print("Как же не удобно набирать код без среды разработки!")
+System.out.print("Как же не удобно набирать код без среды разработки!");
 }
 }
