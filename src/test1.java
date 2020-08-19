9d01d08053cd40d678b4eeead97bf283a4113561
GReeN <andgreen@bk.ru>
2020-08-19 07:13:51 +0300

    fix test 1,3

--- src/test1.java
+++ src/test1.java
@@ -2,5 +2,6 @@ public class test1
 {
     public static void main(String[] args) {
         System.out.printf("Это  лучьшая программа из написанных :)");
+        System.out.println(" + ещё новый код");
     }
 }
