d62e703b1c330eb622954d2fc2cee48546c9e753
Каськов Андрей <andgreen@bk.ru>
2021-09-25 08:17:36 +0300

    new path of training

--- src/test1.java
+++ src/test1.java
@@ -1,7 +1,7 @@
 public class test1
 {
     public static void main(String[] args) {
-        System.out.printf("Это  лучьшая программа из написанных :)");
+        System.out.print("Это  лучьшая программа из написанных :)");
         System.out.println(" + ещё новый код");
     }
 }
--- src/test4.java
+++ src/test4.java
@@ -1,7 +1,5 @@
-public class test4
-{
-public static void main(String[] args)
-{
-System.out.print("Как же не удобно набирать код без среды разработки!");
-}
+public class test4 {
+public static void main(String[] args) {
+    System.out.print("Как же не удобно набирать код без среды разработки!");
+    }
 }
